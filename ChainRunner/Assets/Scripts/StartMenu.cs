73a7dc700cfe5742e5583318e7c4102354cc7061
TaneimMiah <tsm6910@nyu.edu>
2023-04-04 22:18:52 -0400

    StartMenu

---
+++ ChainRunner/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public GameObject continueButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("curLevel"))
+        {
+            continueButton.SetActive(false);
+        } else
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("JessDemo");
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("curLevel"));
+    }
+}
