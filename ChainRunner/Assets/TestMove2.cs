42be1bfff409cb180170e3f150b4f865e577106f
jbekkk <93888734+jbekkk@users.noreply.github.com>
2023-04-22 18:27:02 -0400

    failed attempt at parallaxing y, temple finished, sand made prettier

--- ChainRunner/Assets/TestMove2.cs
+++ ChainRunner/Assets/TestMove2.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBackground : MonoBehaviour
+public class TestMove2 : MonoBehaviour
 {
     private float startPos;
     private float length;
     private GameObject cam;
-    [SerializeField]private float parallaxEffect;
+    [SerializeField]private float parallaxEffectVert;
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
-        startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1-parallaxEffect));
-        float distance = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.y * (1-parallaxEffectVert));
+        float distance = (cam.transform.position.y * parallaxEffectVert);
 
-        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, startPos + distance, transform.position.z);
 
         if (temp > startPos + length)
         {
