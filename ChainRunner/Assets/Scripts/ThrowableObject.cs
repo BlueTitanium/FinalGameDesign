4068447d5bf83dcbb57aa802b7d209f08b9d661b
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-04-25 20:51:34 -0400

    throwable objects optimization

--- ChainRunner/Assets/Scripts/ThrowableObject.cs
+++ ChainRunner/Assets/Scripts/ThrowableObject.cs
@@ -14,17 +14,26 @@ public class ThrowableObject : MonoBehaviour
     public ObjectType type;
     public float damage = 10f;
     public float speed = 20f;
+
+
+    Rigidbody2D rb;
+    Transform playerTransform;
+    float playerDistance;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = transform.parent.GetComponent<Rigidbody2D>();
+        rb.simulated = false;
         GetComponent<SpriteRenderer>().sprite = transform.parent.GetComponent<SpriteRenderer>().sprite;
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        playerDistance = Vector2.Distance(transform.position, playerTransform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (nearObject && Input.GetKeyDown(KeyCode.F))
         {
             if (PlayerController.p.arm.GrabItem(GetComponent<SpriteRenderer>().sprite, type, damage, speed))
@@ -33,6 +42,14 @@ public class ThrowableObject : MonoBehaviour
                 GameManager.g.ItemPickupDisplayToggle(false);
                 Destroy(transform.parent.gameObject);
             }
+        }
+    }
+
+
+    void LateUpdate() {
+        if (!rb.simulated) {
+            playerDistance = Vector2.Distance(transform.position, playerTransform.position);
+            if (playerDistance <= 25) rb.simulated = true;
         }
     }
 
